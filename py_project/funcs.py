b585589375d864b0f8d651474ad83ecd46ccf0a3
annayurkk <yurak.anna@gmail.com>
2023-02-19 20:14:17 +0300

    Add new function.

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -1,8 +1,17 @@
 def sum_digits(a, b):
     result = int(a + b)
     return result
+
+def sub_digits(a, b):
+    result = int(a-b)
+    return result
+
 a = int(input())
 b = int(input())
 print(sum_digits(a, b))
+print(sub_digits(a, b))
+
+
+
 
 
